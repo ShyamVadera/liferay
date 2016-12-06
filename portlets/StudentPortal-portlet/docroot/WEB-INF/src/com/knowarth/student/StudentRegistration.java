61d7951ec8a2b53a04ceedd7f3ff44ccd1671358
shyam.vadera <shyam.vaderaknowarth.com>
2016-12-06 16:26:32 +0530

    no message

--- portlets/StudentPortal-portlet/docroot/WEB-INF/src/com/knowarth/student/StudentRegistration.java
+++ portlets/StudentPortal-portlet/docroot/WEB-INF/src/com/knowarth/student/StudentRegistration.java
@@ -1,11 +1,25 @@
 package com.knowarth.student;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 /**
  * Portlet implementation class StudentRegistration
  */
 public class StudentRegistration extends MVCPortlet {
- 
+	private static final Log log=LogFactoryUtil.getLog(StudentRegistration.class);
+	@Override
+	public void render(RenderRequest arg0, RenderResponse arg1)
+			throws PortletException, IOException {
+		log.info("render method called");
+	
+	}
 
 }
