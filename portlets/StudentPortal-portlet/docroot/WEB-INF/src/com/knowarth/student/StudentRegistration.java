00d0100d4abb48af0d7ccf814b77511119d49e88
shyam.vadera <shyam.vaderaknowarth.com>
2016-12-06 16:10:00 +0530

    New commit

---
+++ portlets/StudentPortal-portlet/docroot/WEB-INF/src/com/knowarth/student/StudentRegistration.java
@@ -0,0 +1,11 @@
+package com.knowarth.student;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class StudentRegistration
+ */
+public class StudentRegistration extends MVCPortlet {
+ 
+
+}
